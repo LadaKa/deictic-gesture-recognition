eb9133d07efba1fa5e70980843770abf4f943fb9
Lada <ladakudlackova@seznam.cz>
2022-07-23 13:46:53 +0200

    Added code to terminate ros_astra_camera streams.

--- code/ros_astra_camera/ros/astra_camera_node.cpp
+++ code/ros_astra_camera/ros/astra_camera_node.cpp
@@ -31,22 +31,46 @@
  */
 
 #include "astra_camera/astra_driver.h"
+#include <std_msgs/Empty.h>
 
+class astra_camera_node
+{
 
-//  TODO: 
-//  add subscriber for topic "stop_object_detection_stream" 
-//  terminate all streams in callback
+private:
+  astra_wrapper::AstraDriver *drv;
+  ros::NodeHandle _nh;
+  ros::Subscriber sub_stop_object_detection_stream;
 
-int main(int argc, char **argv){
+public:
+  void stop_object_detection_stream_cb(const std_msgs::Empty::ConstPtr &msg)
+  {
+    // ~AstraDriver(): 
+    // stop all streams of AstraDriver device
+    drv->~AstraDriver();
+  }
 
-  ROS_INFO("Hallo Spaceboy!");
+  astra_camera_node()
+  {
+    sub_stop_object_detection_stream = _nh.subscribe(
+        "stop_object_detection_stream",
+        1,
+        &astra_camera_node::stop_object_detection_stream_cb,
+        this);
+
+    ros::NodeHandle n;
+    ros::NodeHandle pnh("~");
+
+    drv = new astra_wrapper::AstraDriver(n, pnh);
+    ros::spin();
+  }
+};
+
+int main(int argc, char **argv)
+{
+
+  ROS_INFO("astra_camera_node: Initializing ROS... ");
   ros::init(argc, argv, "astra_camera");
-  ros::NodeHandle n;
-  ros::NodeHandle pnh("~");
-
-  astra_wrapper::AstraDriver drv(n, pnh);
-
-  ros::spin();
+  astra_camera_node();
 
   return 0;
 }
