e8695ec2c2de89ad030e56484ead8f47ebadb220
Lada <ladakudlackova@seznam.cz>
2022-07-24 21:10:15 +0200

    Debugging of objects detection for astra.launch.

--- code/pcl_object_detecton/src/pcl_object_detection_node.cpp
+++ code/pcl_object_detecton/src/pcl_object_detection_node.cpp
@@ -1,3 +1,4 @@
+
 // PCL Object Detection Node
 // Detects objects resting on a Plane
 // Currently, tested with objects on the floor
@@ -9,9 +10,12 @@
 // PCL specific includes
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
+//#include <pcl/conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 
+//#include <pcl/io/pcd_io.h>
+//#include <pcl/point_types.h>
 #include <pcl/common/common.h>
 
 #include <pcl/sample_consensus/model_types.h>
@@ -40,13 +44,16 @@
 // NOTE: you must install TF2 Sensor Messages: sudo apt-get install ros-kinetic-tf2-sensor-msgs
 
 #include "ObjectDetection.h"
-#include "PointCloudPublishers.h"
-#include <std_msgs/Empty.h>
 
 class PclObjectDetection
 {
+public:
+  PclObjectDetection(ros::NodeHandle n);
 
 private:
+  // FUNCTIONS
+  void cloud_cb(const sensor_msgs::PointCloud2ConstPtr &input_cloud_msg);
+
 
   // CONSTANTS
 
@@ -57,6 +64,8 @@ private:
   // VARIABLES
   ros::NodeHandle nh_;
   ros::NodeHandle private_nh_;
+  tf2_ros::Buffer tf2_;
+  tf2_ros::TransformListener tfListener_;
   std::string input_cloud_frame_;
   std::string target_frame_;
   double tf_tolerance_;
@@ -82,83 +91,84 @@ private:
 
   ros::Publisher pub_remaining;
   ros::Publisher pub_objects;
-  ros::Publisher marker_pub;
-
-  ros::Publisher pub_object_detection_done;
+  ros::Publisher marker_pub_;
 
   PointCloudPublishers publishers;
 
-public:
-  PclObjectDetection(ros::NodeHandle n) : nh_(n),
-                                          private_nh_("~"),
-                                          input_cloud_frame_("")
-  {
+  ros::Publisher pub_object_detection_done;
+};
 
-    ROS_INFO("PclObjectDetection: Initializing...");
+PclObjectDetection::PclObjectDetection(ros::NodeHandle n) : nh_(n),
+                                                            private_nh_("~"),
+                                                            tfListener_(tf2_),
+                                                            input_cloud_frame_("")
+{
 
-    // PARAMETERS
-    // TODO USE THESE IN CODE
-    private_nh_.param<std::string>("target_frame", target_frame_, DEFAULT_TARGET_FRAME);
-    private_nh_.param<double>("transform_tolerance", tf_tolerance_, DEFAULT_TF_TOLERANCE);
-    private_nh_.param<std::string>("depth_topic", depth_topic_, DEFAULT_DEPTH_TOPIC);
+  ROS_INFO("PclObjectDetection: Initializing...");
 
-    // PUBLISHERS
-    // Create a ROS publisher for the output point cloud
-    pub_cluster0 = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/cluster0", 1);
-    pub_cluster1 = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/cluster1", 1);
-    pub_cluster2 = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/cluster2", 1);
-    pub_cluster3 = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/cluster3", 1);
-    pub_cluster4 = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/cluster4", 1);
-    pub_nearest_object = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/nearest_object", 1);
+  // PARAMETERS
+  // TODO USE THESE IN CODE
+  private_nh_.param<std::string>("target_frame", target_frame_, DEFAULT_TARGET_FRAME);
+  private_nh_.param<double>("transform_tolerance", tf_tolerance_, DEFAULT_TF_TOLERANCE);
+  private_nh_.param<std::string>("depth_topic", depth_topic_, DEFAULT_DEPTH_TOPIC);
 
-    pub0 = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/plane0", 1);
-    pub1 = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/plane1", 1);
-    pub2 = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/plane2", 1);
-    pub3 = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/plane3", 1);
-    // pub4 = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/plane4", 1);
+  // PUBLISHERS
+  // Create a ROS publisher for the output point cloud
+  pub_cluster0 = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/cluster0", 1);
+  pub_cluster1 = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/cluster1", 1);
+  pub_cluster2 = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/cluster2", 1);
+  pub_cluster3 = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/cluster3", 1);
+  pub_cluster4 = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/cluster4", 1);
+  pub_nearest_object = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/nearest_object", 1);
 
-    pub_voxel = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/voxel_output", 1);
+  pub0 = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/plane0", 1);
+  pub1 = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/plane1", 1);
+  pub2 = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/plane2", 1);
+  pub3 = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/plane3", 1);
+  pub4 = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/plane4", 1);
+  pub_voxel = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/voxel_output", 1);
 
-    pub_remaining = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/remaining", 1);
-    pub_objects = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/objects", 1);
+  pub_remaining = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/remaining", 1);
+  pub_objects = nh_.advertise<sensor_msgs::PointCloud2>("pcl_object_detection/objects", 1);
 
-    // Publish markers to show where robot thinks object is in RViz
-    marker_pub = nh_.advertise<visualization_msgs::Marker>("pcl_object_detection/marker", 1);
 
-    publishers.SetClustersPublishers(
-        pub_cluster0, pub_cluster1, pub_cluster2, pub_cluster3, pub_cluster4);
-    publishers.SetPlanesPublishers(
-        pub0, pub1, pub2, pub3);
-    publishers.SetOtherPublishers(
-        pub_voxel, pub_nearest_object, pub_remaining, pub_objects, marker_pub);
+  // Publish markers to show where robot thinks object is in RViz
+  marker_pub_ = nh_.advertise<visualization_msgs::Marker>("pcl_object_detection/marker", 1);
 
-    pub_object_detection_done = nh_.advertise<std_msgs::Empty>(
+  publishers.SetClustersPublishers(
+      pub_cluster0, pub_cluster1, pub_cluster2, pub_cluster3, pub_cluster4);
+  publishers.SetPlanesPublishers(
+      pub0, pub1, pub2, pub3);
+  publishers.SetOtherPublishers(
+      pub_voxel, pub_nearest_object, pub_remaining, pub_objects, marker_pub_);
+
+  pub_object_detection_done = nh_.advertise<std_msgs::Empty>(
         "pcl_object_detection/object_detection_done", 1);
 
-    // SUBSCRIBERS
-    // Create a ROS subscriber for the input point cloud
-    depth_cloud_sub_ = nh_.subscribe(depth_topic_, 1, &PclObjectDetection::cloud_cb, this);
+  // SUBSCRIBERS
+  // Create a ROS subscriber for the input point cloud
+  depth_cloud_sub_ = nh_.subscribe(depth_topic_, 1, &PclObjectDetection::cloud_cb, this);
 
-    ROS_INFO("PclObjectDetection: Initializing completed.");
-  }
+  ROS_INFO("PclObjectDetection: Initializing completed.");
+}
 
-  void cloud_cb(const sensor_msgs::PointCloud2ConstPtr &input_cloud_msg)
+void PclObjectDetection::cloud_cb(const sensor_msgs::PointCloud2ConstPtr &input_cloud_msg)
+{
+  ROS_INFO("PclObjectDetection: cloud_cb...");
+  input_cloud_frame_ = input_cloud_msg->header.frame_id; 
+  ObjectDetection objectDetection;
+  objectDetection.SetPublishers(publishers);
+
+  if (objectDetection.Detect(
+          input_cloud_msg,
+          target_frame_,
+          tf_tolerance_))
   {
-    ROS_INFO("PclObjectDetection: cloud_cb...");
-    input_cloud_frame_ = input_cloud_msg->header.frame_id; // TF Frame of the point cloud
-    ObjectDetection objectDetection;
-    objectDetection.SetPublishers(publishers);
-
-    if (objectDetection.Detect(
-            input_cloud_msg,
-            target_frame_,
-            tf_tolerance_))
-    {
-      std_msgs::Empty empty_msg;
-      pub_object_detection_done.publish(empty_msg);
-    }
+    ROS_INFO("PclObjectDetection: Objects detected.");
+    std_msgs::Empty empty_msg;
+    pub_object_detection_done.publish(empty_msg);
   }
-};
+}
 
 int main(int argc, char **argv)
 {
